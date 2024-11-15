13303b9c7b968c3a29244b4a5265a00ebda41c13
Yukta Tiwari <tiwariyukta55@gmail.com>
2024-11-15 18:06:48 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,7 @@
+const heading = React.createElement(
+  "h1",
+  { id: "reactHeading" },
+  "Hello From React!"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
